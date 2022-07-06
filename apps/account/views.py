ceb7e784dbbefddf73a961c456ca8e90887c914d
Edige <edige.meticulov@gmail.com>
2022-07-06 18:03:09 +0600

    some changes in account

--- apps/account/views.py
+++ apps/account/views.py
@@ -3,9 +3,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
-from apps.account.serializers import RegistrationSerializer
+from apps.account.serializers import RegistrationSerializer, ForgetPasswordSerializer
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from apps.account.services.utils import send_new_password
 
 User = get_user_model()
 
@@ -29,5 +31,16 @@ class ActivationView(APIView):
         return Response('your account is sucessfully activated!', status=status.HTTP_200_OK)
 
 
-# class LoginView(TokenObtainPairView):
-#     serializer_class = LoginSerializer
+class ForgetPasswordView(APIView):
+
+    def post(self, request):
+        data = request.POST
+        serializer = ForgetPasswordSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data.get('email')
+        user: User = User.objects.get(email=email)
+        new_password = user.generate_activation_code(10, 'qwergafsg57')
+        user.set_password(new_password)
+        user.save()
+        send_new_password(email, new_password)
+        return Response({'message': 'your new password was send to email'}, status=status.HTTP_200_OK)
